447336dfcb3286d6b4b9169b0651e6a196cd08de
secona <77039267+secona@users.noreply.github.com>
2025-03-08 19:18:55 +0700

    feat: implement creating circles

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	rg "github.com/gen2brain/raylib-go/raygui"
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -14,7 +12,26 @@ func main() {
 	rl.InitWindow(800, 450, "kflap")
 	defer rl.CloseWindow()
 
+	var circles []rl.Vector2
+	circleRadius := float32(20)
+
 	for !rl.WindowShouldClose() {
+		mousePos := rl.GetMousePosition()
+
+		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			circles = append(circles, mousePos)
+		}
+
+		if rl.IsMouseButtonPressed(rl.MouseRightButton) {
+			for i := 0; i < len(circles); i++ {
+				if rl.CheckCollisionPointCircle(mousePos, circles[i], circleRadius) {
+					// Remove circle
+					circles = append(circles[:i], circles[i+1:]...)
+					break // Stop after removing one
+				}
+			}
+		}
+
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
@@ -27,16 +44,8 @@ func main() {
 		rg.Button(rl.NewRectangle(150, 50, 40, 40), "Click")
 		rg.Button(rl.NewRectangle(200, 50, 40, 40), "Click")
 
-		if (rl.IsKeyPressed(rl.KeyOne)) {
-			fmt.Println("1")
-		}
-
-		if (rl.IsKeyPressed(rl.KeyTwo)) {
-			fmt.Println("2")
-		}
-
-		if (rl.IsKeyPressed(rl.KeyThree)) {
-			fmt.Println("3")
+		for _, pos := range circles {
+			rl.DrawCircleV(pos, circleRadius, rl.LightGray)
 		}
 
 		rl.EndDrawing()
