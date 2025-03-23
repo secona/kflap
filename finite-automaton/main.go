a7ba1180efba784a1485571c696b20cbe52073b4
secona <77039267+secona@users.noreply.github.com>
2025-03-23 12:44:23 +0700

    feat: finite-automaton module

---
+++ finite-automaton/main.go
@@ -0,0 +1,44 @@
+package finiteautomaton
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func Run() {
+	rl.SetConfigFlags(rl.FlagWindowResizable)
+	rl.SetTargetFPS(60)
+
+	rl.InitWindow(800, 450, "kflap")
+	defer rl.CloseWindow()
+
+	var circles []rl.Vector2
+	circleRadius := float32(20)
+
+	for !rl.WindowShouldClose() {
+		mousePos := rl.GetMousePosition()
+
+		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+			circles = append(circles, mousePos)
+		}
+
+		if rl.IsMouseButtonPressed(rl.MouseRightButton) {
+			for i := 0; i < len(circles); i++ {
+				if rl.CheckCollisionPointCircle(mousePos, circles[i], circleRadius) {
+					// Remove circle
+					circles = append(circles[:i], circles[i+1:]...)
+					break // Stop after removing one
+				}
+			}
+		}
+
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+
+		for _, pos := range circles {
+			rl.DrawCircleV(pos, circleRadius, rl.LightGray)
+		}
+
+		rl.EndDrawing()
+	}
+}
--- main.go
+++ main.go
@@ -1,44 +1,7 @@
 package main
 
-import (
-	rl "github.com/gen2brain/raylib-go/raylib"
-)
+import fa "github.com/secona/kflap/finite-automaton"
 
 func main() {
-	rl.SetConfigFlags(rl.FlagWindowResizable)
-	rl.SetTargetFPS(60)
-
-	rl.InitWindow(800, 450, "kflap")
-	defer rl.CloseWindow()
-
-	var circles []rl.Vector2
-	circleRadius := float32(20)
-
-	for !rl.WindowShouldClose() {
-		mousePos := rl.GetMousePosition()
-
-		if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-			circles = append(circles, mousePos)
-		}
-
-		if rl.IsMouseButtonPressed(rl.MouseRightButton) {
-			for i := 0; i < len(circles); i++ {
-				if rl.CheckCollisionPointCircle(mousePos, circles[i], circleRadius) {
-					// Remove circle
-					circles = append(circles[:i], circles[i+1:]...)
-					break // Stop after removing one
-				}
-			}
-		}
-
-		rl.BeginDrawing()
-
-		rl.ClearBackground(rl.RayWhite)
-
-		for _, pos := range circles {
-			rl.DrawCircleV(pos, circleRadius, rl.LightGray)
-		}
-
-		rl.EndDrawing()
-	}
+	fa.Run()
 }
