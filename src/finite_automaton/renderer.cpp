893249bf59350652194e001eb5433f66f88256df
secona <77039267+secona@users.noreply.github.com>
2025-03-29 22:33:58 +0700

    feat: merge raylib.cpp and renderer.cpp

--- src/finite_automaton/renderer.cpp
+++ src/finite_automaton/renderer.cpp
@@ -1,6 +1,49 @@
+#include <cmath>
+
 #include "finite_automaton/renderer.h"
-#include "finite_automaton/raylib.h"
 #include "finite_automaton/core.h"
+
+void draw_state(State state) {
+  const int fontSize = 14;
+
+  float textW = MeasureText(state.label.c_str(), fontSize);
+  float textH = fontSize;
+
+  int textX = state.position.x - textW / 2;
+  int textY = state.position.y - textH / 2;
+
+  DrawCircleV(state.position, 20, LIGHTGRAY);
+  DrawText(state.label.c_str(), textX, textY, fontSize, BLACK);
+}
+
+void draw_arrow(Vector2 from, Vector2 to) {
+  double head_angle = std::numbers::pi / 4;
+  double head_length = 15;
+
+  double angle = std::atan2(to.y - from.y, to.x - from.x);
+
+  float left_angle = angle + head_angle;
+  float left_x = to.x - std::cos(left_angle) * head_length;
+  float left_y = to.y - std::sin(left_angle) * head_length;
+
+  float right_angle = angle - head_angle;
+  float right_x = to.x - std::cos(right_angle) * head_length;
+  float right_y = to.y - std::sin(right_angle) * head_length;
+
+  DrawLineEx(from, to, 2, BLACK);
+  DrawLineEx(to, Vector2{left_x, left_y}, 2, BLACK);
+  DrawLineEx(to, Vector2{right_x, right_y}, 2, BLACK);
+}
+
+void draw_transition(Transition t) {
+  Vector2 from = t.from.lock()->position;
+  Vector2 to = t.to.lock()->position;
+
+  draw_arrow(from, to);
+
+  char text[] = {t.c, 0};
+  DrawText(text, from.x + (to.x-from.x)/2, from.y + (to.y-from.y)/2, 14, BLACK);
+}
 
 FiniteAutomatonRenderer::FiniteAutomatonRenderer()
     : transition_from(), moving_state(), fa() {}
