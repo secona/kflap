8afa5e9439b5bcc18a496f0da164519ae343d1f2
secona <77039267+secona@users.noreply.github.com>
2025-03-29 22:21:34 +0700

    feat: separate finite automaton renderer

---
+++ include/finite_automaton/renderer.h
@@ -0,0 +1,29 @@
+#ifndef _KFLAP_FINITE_AUTOMATON_RENDERER_H_
+#define _KFLAP_FINITE_AUTOMATON_RENDERER_H_
+
+#include <raylib.h>
+
+#include "finite_automaton/core.h"
+
+#include <optional>
+#include <memory>
+
+enum Tools {
+  TOOL_STATE = KEY_ONE,
+  TOOL_TRANSITION = KEY_TWO,
+  TOOL_MOVE = KEY_THREE,
+};
+
+class FiniteAutomatonRenderer {
+ public:
+  FiniteAutomatonRenderer();
+  void run();
+
+ private:
+  std::optional<std::shared_ptr<State>> transition_from = std::nullopt;
+  std::optional<std::shared_ptr<State>> moving_state = std::nullopt;
+  Tools tool = TOOL_STATE;
+  FiniteAutomaton fa;
+};
+
+#endif /* _KFLAP_FINITE_AUTOMATON_RENDERER_H_ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,9 @@
 #include <raylib.h>
 
-#include <memory>
-#include <optional>
-#include <vector>
-
-#include "finite_automaton/core.h"
-#include "finite_automaton/raylib.h"
-
-enum Tools {
-  TOOL_STATE = KEY_ONE,
-  TOOL_TRANSITION = KEY_TWO,
-  TOOL_MOVE = KEY_THREE,
-};
+#include "finite_automaton/renderer.h"
 
 int main() {
-  std::optional<std::shared_ptr<State>> transition_from = std::nullopt;
-  std::optional<std::shared_ptr<State>> moving_state = std::nullopt;
-  FiniteAutomaton fa;
-
-  Tools tool = TOOL_STATE;
+  FiniteAutomatonRenderer fa;
 
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   SetTargetFPS(60);
@@ -26,93 +11,7 @@ int main() {
   InitWindow(800, 450, "kflap");
 
   while (!WindowShouldClose()) {
-    if (IsKeyPressed(TOOL_STATE)) tool = TOOL_STATE;
-    if (IsKeyPressed(TOOL_TRANSITION)) tool = TOOL_TRANSITION;
-    if (IsKeyPressed(TOOL_MOVE)) tool = TOOL_MOVE;
-
-    BeginDrawing();
-
-    ClearBackground(RAYWHITE);
-
-    switch (tool) {
-      case TOOL_STATE:
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-          fa.add_state(GetMousePosition());
-        }
-
-        if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
-          for (size_t i = 0; i < fa.states.size(); i++) {
-            std::shared_ptr<State> s = fa.states[i];
-            if (CheckCollisionPointCircle(GetMousePosition(), s->position, 20)) {
-              fa.remove_state(s);
-            }
-          }
-        }
-        break;
-
-      case TOOL_TRANSITION:
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-          for (size_t i = 0; i < fa.states.size(); i++) {
-            std::shared_ptr<State> s = fa.states[i];
-            if (CheckCollisionPointCircle(GetMousePosition(), s->position, 20)) {
-              transition_from = s;
-            }
-          }
-        }
-
-        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-          for (size_t i = 0; i < fa.states.size(); i++) {
-            std::shared_ptr<State> s = fa.states[i];
-            if (CheckCollisionPointCircle(GetMousePosition(), s->position, 20)) {
-              fa.add_transition(transition_from.value(), s);
-            }
-          }
-
-          transition_from.reset();
-        }
-
-        if (transition_from.has_value()) {
-          State start = *transition_from.value();
-          draw_arrow(start.position, GetMousePosition());
-        }
-
-        break;
-
-      case TOOL_MOVE:
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-          for (size_t i = 0; i < fa.states.size(); i++) {
-            std::shared_ptr<State> s = fa.states[i];
-            if (CheckCollisionPointCircle(GetMousePosition(), s->position, 20)) {
-              moving_state = s;
-            }
-          }
-        }
-
-        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-          moving_state.reset();
-        }
-
-        if (moving_state.has_value()) {
-          moving_state->get()->position = GetMousePosition();
-        }
-
-        break;
-    }
-
-    for (auto s : fa.states) {
-      draw_state(*s);
-    }
-
-    for (auto t = fa.transitions.begin(); t != fa.transitions.end();) {
-      if (!t->to.expired() && !t->from.expired()) {
-        draw_transition(*t);
-        ++t;
-      } else {
-        t = fa.transitions.erase(t);
-      }
-    }
-
-    EndDrawing();
+    fa.run();
   }
 
   CloseWindow();
