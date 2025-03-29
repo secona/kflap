cbf59b2a838bcb7e2d766f2f21d25f8127f962eb
secona <77039267+secona@users.noreply.github.com>
2025-03-29 10:31:14 +0700

    feat: implement multiple tools

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 #include <raylib.h>
 
+enum Tools {
+	TOOL_STATE = KEY_ONE,
+	TOOL_TRANSITION = KEY_TWO,
+};
+
 class State {
 public:
 	std::string label;
@@ -30,29 +35,41 @@ int main() {
 	int stateCount = 0;
 	std::vector<State> states;
 
+	Tools tool = TOOL_STATE;
+
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	SetTargetFPS(60);
 
 	InitWindow(800, 450, "kflap");
 
 	while (!WindowShouldClose()) {
+		if (IsKeyPressed(TOOL_STATE)) tool = TOOL_STATE;
+		if (IsKeyPressed(TOOL_TRANSITION)) tool = TOOL_TRANSITION;
+
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
 
-		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-			State s(stateCount, GetMousePosition());
-			states.push_back(s);
-			stateCount++;
-		}
+		switch (tool) {
+		case TOOL_STATE:
+			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+				State s(stateCount, GetMousePosition());
+				states.push_back(s);
+				stateCount++;
+			}
 
-		if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
-			for (size_t i = 0; i < states.size(); i++) {
-				State s = states[i];
-				if (CheckCollisionPointCircle(GetMousePosition(), s.position, 20)) {
-					states.erase(states.begin() + i);
+			if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+				for (size_t i = 0; i < states.size(); i++) {
+					State s = states[i];
+					if (CheckCollisionPointCircle(GetMousePosition(), s.position, 20)) {
+						states.erase(states.begin() + i);
+					}
 				}
 			}
+			break;
+
+		case TOOL_TRANSITION:
+			;
 		}
 
 		for (State s : states) {
