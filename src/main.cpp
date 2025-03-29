e90b55c8a7d5043a905080fcd4062546dc7c34a4
secona <77039267+secona@users.noreply.github.com>
2025-03-29 09:57:41 +0700

    feat: add raylib hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <raylib.h>
 
 int main() {
-	std::cout << "Hello, kflap!" << std::endl;
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	SetTargetFPS(60);
+
+	InitWindow(800, 450, "kflap");
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+		DrawText("Hello, world!", 800/2, 450/2, 14, LIGHTGRAY);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
