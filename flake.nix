9c2fbbfb8f2d108bbc0ecbdddfc151d3dae1cb5e
secona <77039267+secona@users.noreply.github.com>
2025-03-30 21:55:36 +0700

    feat: improve flake.nix

--- flake.nix
+++ flake.nix
@@ -3,48 +3,46 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs ,... }: let
+  outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
 
     pkgs = import nixpkgs {
       inherit system;
     };
 
-    kflap = pkgs.stdenv.mkDerivation {
+    stdenv = pkgs.llvmPackages_20.stdenv;
+
+    kflap = stdenv.mkDerivation {
       name = "kflap";
+      version = "0.1.0";
+      src = ./.;
 
       buildInputs = with pkgs; [
         raylib
         meson
         ninja
         pkg-config
-        llvmPackages_20.clang-tools
+        gtest
+        clang-tools
       ];
+
+      buildPhase = ''
+        meson setup build $src
+        meson compile -C build
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp build/kflap $out/bin/
+      '';
     };
   in {
-    devShells."${system}".default = pkgs.mkShell {
+    packages.${system}.default = kflap;
+
+    formatter.${system} = pkgs.alejandra;
+
+    devShells.${system}.default = pkgs.mkShell.override {inherit stdenv;} {
       inputsFrom = [kflap];
-
-      packages = [
-        pkgs.libGL
-
-        # X11
-        pkgs.xorg.libX11
-        pkgs.xorg.libX11.dev
-        pkgs.xorg.libXcursor
-        pkgs.xorg.libXi
-        pkgs.xorg.libXinerama
-        pkgs.xorg.libXrandr
-
-        # Wayland
-        pkgs.wayland
-        pkgs.wayland-protocols
-        pkgs.libxkbcommon
-        pkgs.glfw-wayland
-
-        # Test
-        pkgs.gtest
-      ];
     };
   };
 }
