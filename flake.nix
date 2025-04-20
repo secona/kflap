9818b278fa891ebaae935658683944508b873138
secona <77039267+secona@users.noreply.github.com>
2025-04-20 13:40:49 +0700

    chore(nix): use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,49 +1,52 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-
-    stdenv = pkgs.llvmPackages_20.stdenv;
-
-    kflap = stdenv.mkDerivation {
-      name = "kflap";
-      version = "0.1.0";
-      src = ./.;
-
-      buildInputs = with pkgs; [
-        raylib
-        meson
-        ninja
-        pkg-config
-        gtest
-        clang-tools
-        pugixml
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
       ];
 
-      buildPhase = ''
-        meson setup build $src
-        meson compile -C build
-      '';
+      perSystem = {pkgs, ...}: let
+        stdenv = pkgs.llvmPackages_20.stdenv;
+        mkShell = pkgs.mkShell.override {inherit stdenv;};
 
-      installPhase = ''
-        mkdir -p $out/bin
-        cp build/kflap $out/bin/
-      '';
+        kflap = stdenv.mkDerivation {
+          name = "kflap";
+          version = "0.1.0";
+          src = ./.;
+
+          buildInputs = with pkgs; [
+            raylib
+            meson
+            ninja
+            pkg-config
+            gtest
+            clang-tools
+            pugixml
+          ];
+
+          buildPhase = ''
+            meson setup build $src
+            meson compile -C build
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp build/kflap $out/bin/
+          '';
+        };
+      in {
+        formatter = pkgs.alejandra;
+
+        packages.default = kflap;
+        devShells.default = mkShell {
+          inputsFrom = [kflap];
+        };
+      };
     };
-  in {
-    packages.${system}.default = kflap;
-
-    formatter.${system} = pkgs.alejandra;
-
-    devShells.${system}.default = pkgs.mkShell.override {inherit stdenv;} {
-      inputsFrom = [kflap];
-    };
-  };
 }
